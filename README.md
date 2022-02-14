66f5ba13cd3bdc8c6bf096dfbe23718d8fdb4aa9
tony ho <tho121@hotmail.com>
2022-02-13 16:03:30 -0800

    Adds some csv corner case examples, csv documention links

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Run the file using
 ```
 node .\assignment2.js
 ```
+
+CSV documentation
+
+https://docs.fileformat.com/spreadsheet/csv/
+https://datatracker.ietf.org/doc/html/rfc4180
+
+JSON documentation
+
+https://www.json.org/json-en.html
--- assignment2.js
+++ assignment2.js
@@ -3,24 +3,11 @@ console.log("Hello World");
 var csv2json = require('csv2json');
 var fs = require('fs');
  
-/*
-fs.createReadStream('csv/data.csv')
-    .pipe(csv2json({
-        // Defaults to comma.
-        separator: ';'
-    }))
-    .pipe(fs.createWriteStream('json/data.json'));
 
-fs.createReadStream('csv/data2.csv')
-    .pipe(csv2json())
-    .pipe(fs.createWriteStream('json/data2.json'));
-
-
-fs.createReadStream('csv/no_header.csv')
-    .pipe(csv2json())
-    .pipe(fs.createWriteStream('json/no_header.json'));
-*/
-
-fs.createReadStream('csv/dynamic.csv')
-    .pipe(csv2json({dynamicTyping: true}))
-    .pipe(fs.createWriteStream('json/dynamic.json'));
+//fs.createReadStream('csv/data.csv').pipe(csv2json({separator: ';'})).pipe(fs.createWriteStream('json/data.json'));
+//fs.createReadStream('csv/data2.csv').pipe(csv2json()).pipe(fs.createWriteStream('json/data2.json'));
+//fs.createReadStream('csv/no_header.csv').pipe(csv2json()).pipe(fs.createWriteStream('json/no_header.json'));
+//fs.createReadStream('csv/dynamic.csv').pipe(csv2json({dynamicTyping: true})).pipe(fs.createWriteStream('json/dynamic.json'));
+//fs.createReadStream('csv/dynamic_bounds.csv').pipe(csv2json({dynamicTyping: true})).pipe(fs.createWriteStream('json/dynamic_bounds.json'));
+//fs.createReadStream('csv/quotes1.csv').pipe(csv2json({dynamicTyping: true})).pipe(fs.createWriteStream('json/quotes1.json'));
+fs.createReadStream('csv/line_break.csv').pipe(csv2json({dynamicTyping: true})).pipe(fs.createWriteStream('json/line_break.json'));
