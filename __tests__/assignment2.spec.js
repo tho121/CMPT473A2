984d20d4429daaca4a4c1e17b2939774f10c98da
KunHyung Park <kpa43@sfu.ca>
2022-02-17 16:28:21 -0800

    add test2

--- __tests__/assignment2.spec.js
+++ __tests__/assignment2.spec.js
@@ -14,14 +14,6 @@ let filesManager = new FilesManager();
 //fs.createReadStream('csv/quotes2.csv').pipe(csv2json({dynamicTyping: true})).pipe(fs.createWriteStream('json/quotes2.json'));
 //fs.createReadStream('csv/line_break.csv').pipe(csv2json({dynamicTyping: true})).pipe(fs.createWriteStream('json/line_break.json'));
 
-var isEqualsJson = (obj1,obj2)=>{
-    keys1 = Object.keys(obj1);
-    keys2 = Object.keys(obj2);
-
-    //return true when the two json has same length and all the properties has same value key by key
-    return keys1.length === keys2.length && Object.keys(obj1).every(key=>obj1[key]==obj2[key]);
-}
-
 describe("no header, not double quote enclosed, no dynamic typing, comma separated, tab not as separator, valid input and output path, csv", () => {
     test("Test_1", async() => {
         var expected;
@@ -35,6 +27,24 @@ describe("no header, not double quote enclosed, no dynamic typing, comma separat
         return promise.finally(()=>{
             expected = JSON.parse(fs.readFileSync('expectedOutput/test1.json', 'utf-8'));
             generated = JSON.parse(fs.readFileSync('json/test1.json', 'utf-8'));
+            expect(JSON.stringify(expected)==JSON.stringify(generated)).toEqual(true);
+        });
+    });
+});
+
+describe("Header, double quote enclosed, contains double quotes, dynamic typing, tab as separator, valid input and output path, csv", () => {
+    test("Test_2", async() => {
+        var expected;
+        var generated;
+        
+        var promise = new Promise((resolve, reject) => {
+            var writeStream = fs.createWriteStream('json/test2.json');
+            fs.createReadStream('csv/test2.tab').pipe(csv2json({dynamicTyping: true, separator: '~'})).pipe(writeStream).on('finish', resolve).on('error', reject);
+        });
+
+        return promise.finally(()=>{
+            expected = JSON.parse(fs.readFileSync('expectedOutput/test2.json', 'utf-8'));
+            generated = JSON.parse(fs.readFileSync('json/test2.json', 'utf-8'));
             expect(JSON.stringify(expected)==JSON.stringify(generated)).toEqual(true);
         });
     });
